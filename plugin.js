b85918c05e1f8a469a4c8ed5fabd5c1ee8a36a40
Pavel Kondratenko <p.kondratenko@rambler-co.ru>
2015-11-17 14:45:37 +0300

    add common js support

--- plugin.js
+++ plugin.js
@@ -7,55 +7,61 @@
 
 'use strict';
 
-var $ = require('jquery');
+(function(factory) {
+  if (typeof module === "object" && typeof module.exports === "object") {
+    factory(require("jquery"), window, document);
+  } else {
+    factory(jQuery, window, document);
+  }
+}(function($, window, document, undefined) {
+  var $window = $(window);
 
-var $window = $(window);
+  var defaults = {
+    thresholdEl: null,
+    thresholdOffset: 0,
+    overcomeClass: 'overcomeScroll'
+  };
 
-var defaults = {
-  thresholdEl: null,
-  thresholdOffset: 0,
-  overcomeClass: 'overcomeScroll'
-};
+  $.fn.overcomeScroll = function(options) {
+    return this.each(function() {
+      var $node = $(this);
+      var custom = $.extend({}, defaults, options);
 
-$.fn.overcomeScroll = function(options) {
-  return this.each(function() {
-    var $node = $(this);
-    var custom = $.extend({}, defaults, options);
-
-    function onScrollHandler() {
-      // проверяем, проскроллена ли страница до нужной границы
-      if ($window.scrollTop() >= ((custom.thresholdEl ? custom.thresholdEl.offset().top : 0) + custom.thresholdOffset)) {
-        $node.addClass(custom.overcomeClass);
-      } else {
-        $node.removeClass(custom.overcomeClass);
+      function onScrollHandler() {
+        // проверяем, проскроллена ли страница до нужной границы
+        if ($window.scrollTop() >= ((custom.thresholdEl ? custom.thresholdEl.offset().top : 0) + custom.thresholdOffset)) {
+          $node.addClass(custom.overcomeClass);
+        } else {
+          $node.removeClass(custom.overcomeClass);
+        }
       }
-    }
 
-    // событие - скролл страницы
-    onScrollHandler();
-    $window.on('scroll', $.throttle(onScrollHandler, 100));
-  });
-};
+      // событие - скролл страницы
+      onScrollHandler();
+      $window.on('scroll', throttle(onScrollHandler, 100));
+    });
+  };
 
-$.throttle = function(fn, threshold, scope) {
-  var previous;
-  var deferTimer;
-  var limit = threshold || 250;
+  function throttle(fn, threshold, scope) {
+    var previous;
+    var deferTimer;
+    var limit = threshold || 250;
 
-  return function() {
-    var context = scope || this;
-    var now = Date.now();
-    var args = arguments;
+    return function() {
+      var context = scope || this;
+      var now = Date.now();
+      var args = arguments;
 
-    if (previous && now < previous + limit) {
-      clearTimeout(deferTimer);
-      deferTimer = setTimeout(function() {
+      if (previous && now < previous + limit) {
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function() {
+          previous = now;
+          fn.apply(context, args);
+        }, limit);
+      } else {
         previous = now;
         fn.apply(context, args);
-      }, limit);
-    } else {
-      previous = now;
-      fn.apply(context, args);
-    }
-  };
-};
+      }
+    };
+  }
+}));
